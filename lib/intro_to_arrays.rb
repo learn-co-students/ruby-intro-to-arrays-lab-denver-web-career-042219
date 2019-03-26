86285f8944ee3521b30b43ada386a4bd368241fe
Blake A. Long <blake.a.long@gmail.com>
2019-03-26 14:42:35 -0600

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+    new_array = []
+end
+
+def array_with_two_elements
+    array_with_two_objects = ["one", "two"]
+end
+
+def first_element(array_first)
+    array_first[0]
+end
+
+def third_element(array_third) 
+    array_third[2]
+end
+
+def last_element(array_last) 
+    array_last[-1]
+end
+
+def first_element_with_array_methods(array_no_index_first)
+    array_no_index_first.shift
+end
+
+def last_element_with_array_methods(array_no_index_last) 
+    array_no_index_last.pop
+end
+
+def length_of_array(array_length) 
+    array_length.length
+end
